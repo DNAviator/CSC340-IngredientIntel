ad97ea51d9f1a6af49ccd84f12bc4a22659a6ff3
DNAviator <127952810+DNAviator@users.noreply.github.com>
2024-02-25 13:06:12 -0500

    Made links function

--- IngredientIntel/main/urls.py
+++ IngredientIntel/main/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.index, name="home"),
     path("search/<str:query>", views.search_page, name="search_page"),
     path("results/<str:type>/<str:object>", views.results_page, name="result_page"),
 ]
