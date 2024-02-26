6aa89cab98cb8d005556b384457d19ab71860c70
DNAviator <127952810+DNAviator@users.noreply.github.com>
2024-02-25 20:25:51 -0500

    Set up results page with hardcoded values

--- IngredientIntel/main/urls.py
+++ IngredientIntel/main/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="home"),
     path("search/", views.search_page, name="search_page"),
-    path("results/<int:id>", views.results_page, name="result_page"),
+    path("results/<str:type>/<int:id>", views.results_page, name="result_page"),
     path("login/", views.login, name="login"),
     path("settings/", views.settings, name="settings"),
 
