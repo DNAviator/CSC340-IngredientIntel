5afb39bfef66d3f8932459160549fb4d0dd4b3f3
DNAviator <127952810+DNAviator@users.noreply.github.com>
2024-02-27 12:44:50 -0500

    removed duplicate files

--- IngredientIntel/main/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("search/<str:query>", views.search_page, name="search_page"),
-    path("results/<str:type>/<str:object>", views.result_page, name="result_page"),
-]
